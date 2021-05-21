1760dc88cbd7bcae2912201d562cdfe00c234694
khosyrobbin <khosybin@gmail.com>
2021-05-21 21:10:32 +0700

    add view barang & supplier

---
+++ resources/views/layout/barang.blade.php
@@ -0,0 +1,6 @@
+@extends('layout.template')
+@section('title','Barang')
+
+@section('content')
+    <h1>Halaman Barang</h1>
+@endsection
---
+++ resources/views/layout/supplier.blade.php
@@ -0,0 +1,6 @@
+@extends('layout.template')
+@section('title','Supplier')
+
+@section('content')
+    <h1>Halaman Supplier</h1>
+@endsection
