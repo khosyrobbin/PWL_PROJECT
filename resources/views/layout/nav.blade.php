4f076abadea3deb77170f1c7dea4b92f14091d6e
khosyrobbin <khosybin@gmail.com>
2021-05-25 13:49:39 +0700

    template

---
+++ resources/views/layout/nav.blade.php
@@ -0,0 +1,9 @@
+<ul class="sidebar-menu" data-widget="tree">
+    <li class="header">MENU</li>
+    <li class="{{request()->is('/')?'active': ''}}"><a href="/"><i class="fa fa-home"></i> <span>Home</span></a></li>
+    <li class="{{request()->is('barang')?'active': ''}}"><a href="/barang"><i class="fa fa-user-circle"></i> <span>Data Barang</span></a></li>
+    <li class="{{request()->is('supplier')?'active': ''}}"><a href="/supplier"><i class="fa fa-id-card"></i> <span>Data Supplier</span></a></li>
+    <li class="{{request()->is('trasaksi')?'active': ''}}"><a href="/trasaksi"><i class="fa fa-book"></i> <span>Data Transaksi</span></a></li>
+    <li class="{{request()->is('pembayaran')?'active': ''}}"><a href="/pembayaran"><i class="fa fa-calendar"></i> <span>Data Pembayaran</span></a></li>
+    <li class="{{request()->is('pemebeli')?'active': ''}}"><a href="/pemebeli"><i class="fa fa-clipboard"></i> <span>Data Pembeli</span></a></li>
+</ul>
